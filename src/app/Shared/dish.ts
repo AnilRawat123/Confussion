afdf298b6cb53b81f49c0518a8bf4764d3b1f24d
Anil Rawat <anilrawatyashika@gmail.com>
2022-01-17 19:41:27 +0530

    Add

---
+++ src/app/Shared/dish.ts
@@ -0,0 +1,12 @@
+export class Dish
+{
+    id!: string;
+    name!:string;
+    image!:string;
+    category!:string;
+    feature!:string;
+    lable!:string;
+    price!:string;
+    description!:string;
+    comments: any[] | undefined;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
+import { DishdetailComponent } from './dishdetail/dishdetail.component'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
